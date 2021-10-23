93906c7ec2afe819ed978d9994fec32be6833cd1
Melchiah <melchiahmauck@gmail.com>
2021-10-23 16:08:29 -0700

    const Event won't throw errors if someone tries to change its listenners.

--- lib/OtherLibraries/Event.dart
+++ lib/OtherLibraries/Event.dart
@@ -1,20 +1,33 @@
 import 'dart:developer';
+import 'dart:collection' as D_Collections;
 
 class Event {
   final List<Function?> listeners;
+
+  bool get listenersIsModifiable {
+    bool isModifiable = true;
+    try {
+      listeners.add(null);
+    } catch (e) {
+      isModifiable = false;
+    }
+    return isModifiable;
+  }
 
   Event() : listeners = [];
 
   const Event.unchanging() : listeners = const [];
 
   void addListener(Function? listener) {
-    if (listener != null) {
+    if (listener != null && listenersIsModifiable) {
       listeners.add(listener);
     }
   }
 
   void removeListener(Function? listener) {
-    listeners.remove(listener);
+    if (listenersIsModifiable) {
+      listeners.remove(listener);
+    }
   }
 
   void trigger() {
