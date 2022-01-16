52e558c768011d24d7a6cd9ec5016e387aafa3b9
Melchiah <melchiahmauck@gmail.com>
2022-01-15 16:43:46 -0800

    Made boolean operations a little more flexible.

--- lib/Utilities/BooleanGetterOperations.dart
+++ lib/Utilities/BooleanGetterOperations.dart
@@ -1,6 +1,6 @@
 part of tke_item_store;
 
-Getter<bool> areEqual<ValueType>(Getter<ValueType> getterA, Getter<ValueType> getterB) => Computed(
+Getter<bool> areEqual<ValueTypeA, ValueTypeB>(Getter<ValueTypeA> getterA, Getter<ValueTypeB> getterB) => Computed(
   () => getterA.value == getterB.value,
   recomputeTriggers: [
     getterA.onAfterChange,
@@ -8,7 +8,7 @@ Getter<bool> areEqual<ValueType>(Getter<ValueType> getterA, Getter<ValueType> ge
   ],
 );
 
-Getter<bool> areNotEqual<ValueType>(Getter<ValueType> getterA, Getter<ValueType> getterB) => Computed(
+Getter<bool> areNotEqual<ValueTypeA, ValueTypeB>(Getter<ValueTypeA> getterA, Getter<ValueTypeB> getterB) => Computed(
   () => getterA.value != getterB.value,
   recomputeTriggers: [
     getterA.onAfterChange,
